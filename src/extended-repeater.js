c832f27c6a349fc05f7693905600f58baa089184
PrianiKate <li.liliana@yandex.ru>
2021-01-25 18:15:17 +0400

    feat: extended-repeater and simple-chain

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  str = (str === null) ? 'null' : str.toString();
+  let repeatTimes = options['repeatTimes'] ? options['repeatTimes'] : 1;
+  let separator = options.hasOwnProperty('separator') ? options['separator'] : '+';
+  let addSeparator = options.hasOwnProperty('additionSeparator') ? options['additionSeparator'] : '|';
+  let addition = options.hasOwnProperty('addition') ? options['addition'] : '';
+  let addRepeatTimes = (options.hasOwnProperty('additionRepeatTimes') && options['additionRepeatTimes']) ? options['additionRepeatTimes'] : 1;
+  let ans = '';
+  for (let i = 0; i < repeatTimes; i++) {
+    if (i !== 0) ans += separator;
+    ans += str;
+    for (let j = 0; j < addRepeatTimes; j++) {
+      if (j !== 0) ans += addSeparator;
+      ans += addition;
+    }
+  }
+  return ans;
 };
   
--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,66 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  length: 0,
+  chain: '',
+  obj: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (this.getLength() === 0) {
+      this.chain = `( ${value} )`;
+    } else {
+      this.chain += `~~( ${value} )`;
+    }
+    this.obj.push(value);
+    this.length++;
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (!typeof(position) === 'number' 
+      || position > this.getLength()
+      || position < 1) {
+      this.deleteChain();
+      throw Error;
+    }
+    let l = 0;
+    let r = this.chain.indexOf(')');
+    for (let i = 1; i <= this.getLength(); i++) 
+      if (i === position) {
+        if (i === 1 || i === this.getLength())
+          this.chain = this.chain.substring(0, l) + this.chain.substring(r + 1, this.chain.length);
+        else
+        this.chain = this.chain.substring(0, l) + this.chain.substring(r + 3, this.chain.length);
+        break;
+      } else {
+        l = this.chain.indexOf('(', r + 1);
+        r = this.chain.indexOf(')', r + 1);
+      }
+    this.length--;
+    this.obj.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.obj.reverse();
+    let arr = [];
+    for (let i = 0; i < this.obj.length; i++)
+      arr[i] = this.obj[i];
+    let len = this.length;
+    this.deleteChain();
+    for (let i = 0; i < len; i++)
+        this.addLink(arr[i]);
+    return this;
+  },
+  deleteChain() {
+    this.length = 0;
+    this.chain = '';
+    this.obj.length = 0;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let ans = this.chain;
+    this.deleteChain();
+    return ans;
   }
 };
 
